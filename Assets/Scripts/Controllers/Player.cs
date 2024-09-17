0f607fb9cd5ced3740781037b84bc0a44a1c82e1
JacobMunterSGD <148461541+JacobMunterSGD@users.noreply.github.com>
2024-09-17 14:09:32 -0400

    Added Player Movement
    implemented playermovement method

--- Assets/Scripts/Controllers/Player.cs
+++ Assets/Scripts/Controllers/Player.cs
@@ -9,9 +9,35 @@ public class Player : MonoBehaviour
     public GameObject bombPrefab;
     public Transform bombsTransform;
 
+    public float moveSpeed;
+
     void Update()
     {
+        if (Input.GetKey("up"))
+        {
+            PlayerMovement(0, moveSpeed * Time.deltaTime);
+        }
 
+        if (Input.GetKey("down"))
+        {
+            PlayerMovement(0, -moveSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey("right"))
+        {
+            PlayerMovement(moveSpeed * Time.deltaTime, 0);
+        }
+
+        if (Input.GetKey("left"))
+        {
+            PlayerMovement(-moveSpeed * Time.deltaTime, 0);
+        }
+    }
+
+    void PlayerMovement(float x, float y)
+    {
+        Vector3 velocity = new Vector3(x, y, 0);
+        transform.position += velocity;
     }
 
 }
