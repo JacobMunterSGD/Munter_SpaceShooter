02a13dec08b889be47a3e66dbd0322e3ae0047a5
JacobMunterSGD <148461541+JacobMunterSGD@users.noreply.github.com>
2024-09-19 14:25:15 -0400

    Task 4 - constellation
    lines get drawn over time between the stars

--- Assets/Scripts/Controllers/Stars.cs
+++ Assets/Scripts/Controllers/Stars.cs
@@ -7,7 +7,47 @@ public class Stars : MonoBehaviour
     public List<Transform> starTransforms;
     public float drawingTime;
 
+    int starPosition;
+    int intervalPoint;
+    Vector3 interval;
+
+    Vector3 differenceBetweenPoints;
+
     void Update()
     {
+        DrawConstellation();
+    }
+
+    public void DrawConstellation()
+    {
+
+        if (starPosition <= 0 || starPosition > starTransforms.Count - 1)
+        {
+            starPosition = 1;
+            differenceBetweenPoints = starTransforms[starPosition].position - starTransforms[starPosition - 1].position;
+        }
+        
+        interval = differenceBetweenPoints / drawingTime * 0.02f;
+
+        Vector3 drawTo = starTransforms[starPosition - 1].position + interval * intervalPoint;
+
+        Debug.DrawLine(starTransforms[starPosition - 1].position, drawTo);
+
+        print(drawTo);
+
+        if (drawTo - starTransforms[starPosition - 1].position == differenceBetweenPoints)
+        {
+            starPosition++;
+            intervalPoint = 0;
+            print("true");
+            if (starPosition > starTransforms.Count - 1)
+                starPosition = 1;
+            differenceBetweenPoints = starTransforms[starPosition].position - starTransforms[starPosition - 1].position;
+        }
+        else
+        {
+            intervalPoint++;
+        }
+
     }
 }
