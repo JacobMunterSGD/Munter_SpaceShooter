9a54e05c2760fa7cfda4d374fdc5dc5764263aea
JacobMunterSGD <148461541+JacobMunterSGD@users.noreply.github.com>
2024-09-25 14:35:45 -0400

    Completed Journal 4 - spin to win

--- Assets/Scripts/Controllers/Moon.cs
+++ Assets/Scripts/Controllers/Moon.cs
@@ -6,15 +6,60 @@ public class Moon : MonoBehaviour
 {
     public Transform planetTransform;
 
-    // Start is called before the first frame update
-    void Start()
+    List<float> degrees = new List<float>();
+    List<Vector3> orbitSteps = new List<Vector3>();
+
+    int stepNumber;
+
+    public float waitFor;
+
+    private void Start()
     {
-        
+        OrbitalMotionSetup(3, 5, planetTransform);
     }
 
-    // Update is called once per frame
     void Update()
     {
+        OrbitalMotion();
+    }
+
+    public void OrbitalMotionSetup(float radius, float speed, Transform target)
+    {
+        float circumference = 2 * Mathf.PI * radius;
+        float timeToOrbit = circumference / speed;
+        float framesToOrbit = timeToOrbit * 60;
+
+        float intervalPerAngle = 360 / framesToOrbit;
+
+        for (int i = 0; i < framesToOrbit; i++)
+        {
+            degrees.Add(i * intervalPerAngle);
+        }
+
+        foreach (float d in degrees)
+        {
+            orbitSteps.Add(new Vector3(target.position.x + Mathf.Cos(d * Mathf.Deg2Rad) * radius, target.position.y + Mathf.Sin(d * Mathf.Deg2Rad) * radius, 0));
+        }
+
+        stepNumber = orbitSteps.Count - 1;
 
     }
+
+    public void OrbitalMotion()
+    {
+
+        transform.position = orbitSteps[stepNumber];
+
+        if (stepNumber - 1 < 0)
+        {
+            OrbitalMotionSetup(3, 5, planetTransform);
+            return;
+        }
+        else
+        {
+            stepNumber--;
+        }
+
+    }
+
 }
--- Assets/Scripts/Controllers/Player.cs
+++ Assets/Scripts/Controllers/Player.cs
@@ -1,13 +1,17 @@
-﻿using System.Collections;
+﻿using Codice.CM.Common;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 public class Player : MonoBehaviour
 {
     public List<Transform> asteroidTransforms;
-    public Transform enemyTransform;
+    public GameObject enemyTransform;
     public GameObject bombPrefab;
     public Transform bombsTransform;
+
+    public GameObject powerup;
 
     public float moveSpeed;
 
@@ -43,8 +47,83 @@ public class Player : MonoBehaviour
             PlayerDeceleration();
         }
 
+        if (Input.GetKeyDown("p"))
+        {
+            SpawnPowerups(4, 3);
+        }
+
         transform.position += velocity * Time.deltaTime;
-        
+
+        EnemyRadar(2, 20);
+
+    }
+
+    public void SpawnPowerups(float radius, int numberOfPowerups)
+    {
+        List<float> degrees = new List<float>();
+        float intervalPerAngle = 360 / numberOfPowerups;
+
+        for (int i = 0; i < numberOfPowerups; i++)
+        {
+            degrees.Add(i * intervalPerAngle);
+        }
+
+        List<Vector3> powerUpSpawnPoints = new List<Vector3>();
+
+        foreach (float d in degrees)
+        {
+            powerUpSpawnPoints.Add(new Vector3(transform.position.x + Mathf.Cos(d * Mathf.Deg2Rad) * radius, transform.position.y + Mathf.Sin(d * Mathf.Deg2Rad) * radius, 0));
+        }
+
+        foreach (Vector3 p in powerUpSpawnPoints)
+        {
+            Instantiate(powerup, p, Quaternion.identity);
+        }
+
+    }
+
+    public void EnemyRadar(float radius, int circlePoints)
+    {
+        List<float> degrees = new List<float>();
+        float intervalPerAngle = 360 / circlePoints;
+
+        for (int i = 0; i < circlePoints; i ++)
+        {
+            degrees.Add(i * intervalPerAngle);
+        }
+
+        List<Vector3> polygonPoints = new List<Vector3>();
+
+        foreach (float d in degrees)
+        {
+            polygonPoints.Add(new Vector3(transform.position.x + Mathf.Cos(d * Mathf.Deg2Rad) * radius, transform.position.y + Mathf.Sin(d * Mathf.Deg2Rad) * radius, 0));
+        }
+
+        Color lineColor = Color.green;
+
+        if (Vector3.Magnitude(enemyTransform.transform.position - transform.position) < radius)
+        {
+            lineColor = Color.red;
+        }
+
+
+        Vector3 lastPoint = Vector3.zero;
+
+        foreach (Vector3 p in polygonPoints)
+        {
+            if (lastPoint != Vector3.zero)
+            {
+                Debug.DrawLine(lastPoint, p, lineColor);
+            }
+            else
+            {
+                Debug.DrawLine(p, polygonPoints[polygonPoints.Count - 1], lineColor);
+            }
+            lastPoint = p;
+        }
+
+
+
     }
 
     void PlayerMovement(float x, float y)
